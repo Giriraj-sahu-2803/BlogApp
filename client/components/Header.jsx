bd3fd02d9248dde389d9b85c974f83406339ef6c
girirajSahu07 <grajs280303@gmail.com>
2023-02-13 17:36:55 +0530

    minor configurations

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <div className="md:float-left  inline-block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white ">
-              GraphCMS
+              Glance
             </span>
           </Link>
         </div>
